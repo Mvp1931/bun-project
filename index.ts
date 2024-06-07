52f443409114f0234890c6e3179d52349f2e31cb
Mvp1931 <mihirphadnis001@gmail.com>
2024-06-07 06:51:24 +0000

    created server code.

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+const PORT= 9000
+
+const server = Bun.serve({
+    port: PORT,
+    fetch (req) {
+        return new Response(`Hello from bun server at Port ${this.port}:: new message`);
+    }
+});
+
+console.log(`Listening on Port https://localhost:${server.port}`);
